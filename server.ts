57e2706503ab142b560a334a50de49ca52a3a8ed
Daskal1994 <daskalsteven117@gmail.com>
2024-06-01 14:48:22 -0500

    🔧 (.gitignore): añadir archivos y directorios a ignorar ✨ (play.ts): añadir comandos de reproducción de música para un bot de Discord ✨ (main.ts): añadir lógica para manejar comandos de Discord y reproducir música ✨ (package.json): inicializar archivo de configuración del proyecto ✨ (server.ts): inicializar archivo de configuración del servidor Se agregaron archivos y directorios al archivo .gitignore para ignorar en el control de versiones. Se añadieron comandos de reproducción de música para un bot de Discord en play.ts. Se implementó la lógica para manejar comandos de Discord y reproducir música en main.ts. Se inicializaron los archivos de configuración del proyecto en package.json y del servidor en server.ts.

---
+++ server.ts
@@ -0,0 +1,12 @@
+{
+    "name": "nodemon",
+    "version": "1.0.0",
+    "description": "",
+    "main": "server.js",
+    "scripts": {
+      "start": "node src/server.js"
+    },
+    "keywords": [],
+    "author": "Francisco Suarez <imanolsuarez98@gmail.com>",
+    "license": "MIT"
+  }
