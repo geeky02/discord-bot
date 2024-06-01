e78fbd69cb61ccec4cbd879407b3785553f9b907
Daskal1994 <daskalsteven117@gmail.com>
2024-06-01 15:10:47 -0500

    📝 (.gitignore): añadir yarn.lock al archivo .gitignore 🔧 (nodemon.json): añadir archivo de configuración nodemon.json para supervisar cambios en la carpeta src y ejecutar ts-node src/main.ts 🔧 (package.json): actualizar scripts dev, dev-win, build y serve para facilitar el desarrollo y la compilación del proyecto ♻️ (play.ts): comentar código no utilizado en el archivo play.ts para mejorar la legibilidad y mantener el código limpio.
    ♻️ (main.ts): Refactorizar el código eliminando comentarios y líneas de código innecesarias
    Se refactorizó el código eliminando comentarios y líneas de código innecesarias para mejorar la legibilidad y mantener el código limpio. Esto facilita la comprensión del código y su mantenimiento en el futuro.

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -1,114 +1,114 @@
-const { SlashCommandBuilder } = require("@discordjs/builders")
-const { EmbedBuilder } = require("discord.js")
-const { QueryType } = require("discord-player")
+// const { SlashCommandBuilder } = require("@discordjs/builders")
+// const { EmbedBuilder } = require("discord.js")
+// const { QueryType } = require("discord-player")
 
-module.exports = {
-	data: new SlashCommandBuilder()
-		.setName("play")
-		.setDescription("play a song from YouTube.")
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName("search")
-				.setDescription("Searches for a song and plays it")
-				.addStringOption(option =>
-					option.setName("searchterms").setDescription("search keywords").setRequired(true)
-				)
-		)
-        .addSubcommand(subcommand =>
-			subcommand
-				.setName("playlist")
-				.setDescription("Plays a playlist from YT")
-				.addStringOption(option => option.setName("url").setDescription("the playlist's url").setRequired(true))
-		)
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName("song")
-				.setDescription("Plays a single song from YT")
-				.addStringOption(option => option.setName("url").setDescription("the song's url").setRequired(true))
-		),
-	execute: async ({ client, interaction }) => {
-        // Make sure the user is inside a voice channel
-        
-		if (!interaction.member.voice.channel) return interaction.reply("You need to be in a Voice Channel to play a song.");
+// module.exports = {
+// 	data: new SlashCommandBuilder()
+// 		.setName("play")
+// 		.setDescription("play a song from YouTube.")
+// 		.addSubcommand(subcommand =>
+// 			subcommand
+// 				.setName("search")
+// 				.setDescription("Searches for a song and plays it")
+// 				.addStringOption(option =>
+// 					option.setName("searchterms").setDescription("search keywords").setRequired(true)
+// 				)
+// 		)
+//         .addSubcommand(subcommand =>
+// 			subcommand
+// 				.setName("playlist")
+// 				.setDescription("Plays a playlist from YT")
+// 				.addStringOption(option => option.setName("url").setDescription("the playlist's url").setRequired(true))
+// 		)
+// 		.addSubcommand(subcommand =>
+// 			subcommand
+// 				.setName("song")
+// 				.setDescription("Plays a single song from YT")
+// 				.addStringOption(option => option.setName("url").setDescription("the song's url").setRequired(true))
+// 		),
+// 	execute: async ({ client, interaction }) => {
+//         // Make sure the user is inside a voice channel
 
-        // Create a play queue for the server
-		const queue = await client.player.nodes.create(interaction.guild);
+// 		if (!interaction.member.voice.channel) return interaction.reply("You need to be in a Voice Channel to play a song.");
 
-        // Wait until you are connected to the channel
-		if (!queue.connection) await queue.connect(interaction.member.voice.channel)
+//         // Create a play queue for the server
+// 		const queue = await client.player.nodes.create(interaction.guild);
 
-		let embed = new EmbedBuilder()
+//         // Wait until you are connected to the channel
+// 		if (!queue.connection) await queue.connect(interaction.member.voice.channel)
 
-		if (interaction.options.getSubcommand() === "song") {
-            let url = interaction.options.getString("url")
-            
-            // Search for the song using the discord-player
-            const result = await client.player.search(url, {
-                requestedBy: interaction.user,
-                searchEngine: QueryType.YOUTUBE_VIDEO
-            })
+// 		let embed = new EmbedBuilder()
 
-            // finish if no tracks were found
-            if (result.tracks.length === 0)
-                return interaction.reply("No results")
+// 		if (interaction.options.getSubcommand() === "song") {
+//             let url = interaction.options.getString("url")
 
-            // Add the track to the queue
-            const song = result.tracks[0]
-            await queue.addTrack(song)
-            embed
-                .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
-                .setThumbnail(song.thumbnail)
-                .setFooter({ text: `Duration: ${song.duration}`})
+//             // Search for the song using the discord-player
+//             const result = await client.player.search(url, {
+//                 requestedBy: interaction.user,
+//                 searchEngine: QueryType.YOUTUBE_VIDEO
+//             })
 
-		}
-        else if (interaction.options.getSubcommand() === "playlist") {
+//             // finish if no tracks were found
+//             if (result.tracks.length === 0)
+//                 return interaction.reply("No results")
 
-            // Search for the playlist using the discord-player
-            let url = interaction.options.getString("url")
-            const result = await client.player.search(url, {
-                requestedBy: interaction.user,
-                searchEngine: QueryType.YOUTUBE_PLAYLIST
-            })
+//             // Add the track to the queue
+//             const song = result.tracks[0]
+//             await queue.addTrack(song)
+//             embed
+//                 .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
+//                 .setThumbnail(song.thumbnail)
+//                 .setFooter({ text: `Duration: ${song.duration}`})
 
-            if (result.tracks.length === 0)
-                return interaction.reply(`No playlists found with ${url}`)
-            
-            // Add the tracks to the queue
-            const playlist = result.playlist
-            await queue.addTracks(result.tracks)
-            embed
-                .setDescription(`**${result.tracks.length} songs from [${playlist.title}](${playlist.url})** have been added to the Queue`)
-                .setThumbnail(playlist.thumbnail)
+// 		}
+//         else if (interaction.options.getSubcommand() === "playlist") {
 
-		} 
-        else if (interaction.options.getSubcommand() === "search") {
+//             // Search for the playlist using the discord-player
+//             let url = interaction.options.getString("url")
+//             const result = await client.player.search(url, {
+//                 requestedBy: interaction.user,
+//                 searchEngine: QueryType.YOUTUBE_PLAYLIST
+//             })
 
-            // Search for the song using the discord-player
-            let url = interaction.options.getString("searchterms")
-            const result = await client.player.search(url, {
-                requestedBy: interaction.user,
-                searchEngine: QueryType.AUTO
-            })
+//             if (result.tracks.length === 0)
+//                 return interaction.reply(`No playlists found with ${url}`)
 
-            // finish if no tracks were found
-            if (result.tracks.length === 0)
-                return interaction.editReply("No results")
-            
-            // Add the track to the queue
-            const song = result.tracks[0]
-            await queue.addTrack(song)
-            embed
-                .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
-                .setThumbnail(song.thumbnail)
-                .setFooter({ text: `Duration: ${song.duration}`})
-		}
+//             // Add the tracks to the queue
+//             const playlist = result.playlist
+//             await queue.addTracks(result.tracks)
+//             embed
+//                 .setDescription(`**${result.tracks.length} songs from [${playlist.title}](${playlist.url})** have been added to the Queue`)
+//                 .setThumbnail(playlist.thumbnail)
 
-        // Play the song
-        if (!queue.playing) await queue.play()
-        
-        // Respond with the embed containing information about the player
-        await interaction.reply({
-            embeds: [embed]
-        })
-	},
-}
+// 		}
+//         else if (interaction.options.getSubcommand() === "search") {
+
+//             // Search for the song using the discord-player
+//             let url = interaction.options.getString("searchterms")
+//             const result = await client.player.search(url, {
+//                 requestedBy: interaction.user,
+//                 searchEngine: QueryType.AUTO
+//             })
+
+//             // finish if no tracks were found
+//             if (result.tracks.length === 0)
+//                 return interaction.editReply("No results")
+
+//             // Add the track to the queue
+//             const song = result.tracks[0]
+//             await queue.addTrack(song)
+//             embed
+//                 .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
+//                 .setThumbnail(song.thumbnail)
+//                 .setFooter({ text: `Duration: ${song.duration}`})
+// 		}
+
+//         // Play the song
+//         if (!queue.playing) await queue.play()
+
+//         // Respond with the embed containing information about the player
+//         await interaction.reply({
+//             embeds: [embed]
+//         })
+// 	},
+// }
--- src/main.ts
+++ src/main.ts
@@ -1,71 +1,72 @@
-require('dotenv').config();
+// require("dotenv").config();
 
-const {REST} = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { Client, Collection } = require('discord.js');
-const { Player } = require("discord-player")
+// const { REST } = require("@discordjs/rest");
+// const { Routes } = require("discord-api-types/v9");
+// const { Client, Collection } = require("discord.js");
+// const { Player } = require("discord-player");
 
-const fs = require('fs');
-const path = require('path');
+// const fs = require("fs");
+// const path = require("path");
 
+// const client = new Client({
+//   intents: ["Guilds", "GuildMessages", "GuildVoiceStates"],
+// });
 
-const client = new Client({
-    intents: ["Guilds", "GuildMessages", "GuildVoiceStates"]
-});
+// const commands = [];
+// client.commands = new Collection();
 
-const commands = [];
-client.commands = new Collection();
+// const commandsPath = path.join(__dirname, "commands"); // E:\yt\discord bot\js\intro\commands
+// const commandFiles = fs
+//   .readdirSync(commandsPath)
+//   .filter((file) => file.endsWith(".js"));
+// for (const file of commandFiles) {
+//   const filePath = path.join(commandsPath, file);
+//   const command = require(filePath);
 
-const commandsPath = path.join(__dirname, "commands"); // E:\yt\discord bot\js\intro\commands
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-for(const file of commandFiles)
-{
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
+//   client.commands.set(command.data.name, command);
+//   commands.push(command.data.toJSON());
+// }
 
-    client.commands.set(command.data.name, command);
-    commands.push(command.data.toJSON());
-}
+// // Add the player on the client
+// client.player = new Player(client, {
+//   ytdlOptions: {
+//     quality: "highestaudio",
+//     highWaterMark: 1 << 25,
+//   },
+// });
 
-// Add the player on the client
-client.player = new Player(client, {
-    ytdlOptions: {
-        quality: "highestaudio",
-        highWaterMark: 1 << 25
-    }
-})
+// client.on("ready", () => {
+//   // Get all ids of the servers
+//   const guild_ids = client.guilds.cache.map((guild) => guild.id);
 
-client.on("ready", () => {
-    // Get all ids of the servers
-    const guild_ids = client.guilds.cache.map(guild => guild.id);
+//   const rest = new REST({ version: "9" }).setToken(process.env.DC_BOT_TOKEN);
+//   for (const guildId of guild_ids) {
+//     rest
+//       .put(Routes.applicationGuildCommands(process.env.DC_APP_ID, guildId), {
+//         body: commands,
+//       })
+//       .then(() =>
+//         console.log("Successfully updated commands for guild " + guildId)
+//       )
+//       .catch(console.error);
+//   }
+// });
 
+// client.on("interactionCreate", async (interaction) => {
+//   if (!interaction.isCommand()) return;
 
-    const rest = new REST({version: '9'}).setToken(process.env.DC_BOT_TOKEN);
-    for (const guildId of guild_ids)
-    {
-        rest.put(Routes.applicationGuildCommands(process.env.DC_APP_ID, guildId), 
-            {body: commands})
-        .then(() => console.log('Successfully updated commands for guild ' + guildId))
-        .catch(console.error);
-    }
-});
+//   const command = client.commands.get(interaction.commandName);
 
-client.on("interactionCreate", async interaction => {
-    if(!interaction.isCommand()) return;
+//   if (!command) return;
 
-    const command = client.commands.get(interaction.commandName);
+//   try {
+//     await command.execute({ client, interaction });
+//   } catch (error) {
+//     console.error(error);
+//     await interaction.reply({
+//       content: "There was an error executing this command",
+//     });
+//   }
+// });
 
-    if(!command) return;
-
-    try
-    {   
-        await command.execute({client, interaction});
-    }
-    catch(error)
-    {
-        console.error(error);
-        await interaction.reply({content: "There was an error executing this command"});
-    }
-});
-
-client.login(process.env.DC_BOT_TOKEN);
+// client.login(process.env.DC_BOT_TOKEN);
